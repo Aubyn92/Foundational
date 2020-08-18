e0c2cf7515cc6ff7ead4865b2c48d5cdf73ead55
Aubyn92 <livstaubyn92@gmail.com>
2020-08-18 11:14:24 +1000

    Task 2 completed for asking user for their name and then greeting them with that name. Use of generic collections incorporated.

--- basic-coding/Program.cs
+++ basic-coding/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace basic_coding
 {
@@ -6,7 +7,13 @@ namespace basic_coding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Task 1: Write a program that prints ‘Hello World’ to the screen:
+            // Console.WriteLine("Hello World!");
+
+            // Task 2: Write a program that asks the user for their name and greets them with their name:
+               Console.WriteLine("Please enter your name: ");
+               string name = Console.ReadLine();
+               Console.WriteLine($"Hello, {name}!");
         }
     }
 }
