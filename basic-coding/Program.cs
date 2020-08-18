7b2f23ac5de06959ad380712236662af55ccdc7c
Aubyn92 <livstaubyn92@gmail.com>
2020-08-18 17:25:14 +1000

    Task 8 guessing game; still needs last bit of logic added to prevent the same numbers input by users counting as one try.

--- basic-coding/Program.cs
+++ basic-coding/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace basic_coding
 {
@@ -79,12 +82,63 @@ namespace basic_coding
             //    Console.ReadLine();
 
             // Task 7: Write a program that prints a multiplication table for numbers up to 12:
-            int num;
-            Console.WriteLine("Enter a number: ");
-            num = Convert.ToInt32(Console.ReadLine());
-            for(int i = 1; i <= 12; i++)
+            //    int num;
+            //    Console.WriteLine("Enter a number: ");
+            //    num = Convert.ToInt32(Console.ReadLine());
+            //    for(int i = 1; i <= 12; i++)
+            //    {
+            //       Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
+            //    }
+
+            // Task 8: Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively:
+            int randomNumber = new Random().Next(1, 101); //generates a random number between 1 and 100
+            int inputGuess; //stores users input
+            int countInputGuess = 0; //keeps count of guesses
+            Boolean userWin = false; //default value is false
+
+            while (userWin != true)
             {
-                Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
+                Console.WriteLine("Take your guess between 1 and 100: ");
+                countInputGuess++;
+
+                var userInput = Console.ReadLine();
+                if (validateInput(userInput))
+                {
+                    Console.WriteLine(evaluateAnswer());
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid number.");
+                }
+
+                Boolean validateInput(String userInput)
+                {
+                    if (int.TryParse(userInput, out inputGuess))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                String evaluateAnswer()
+                {
+                    if (inputGuess == randomNumber)
+                    {
+                        userWin = true;
+                        return $"You've won, yay! The number is { randomNumber }. You attempted it { countInputGuess } times. Woop woop! \n";
+                    }
+                    else if (inputGuess < randomNumber)
+                        {
+                            return $"Too low! You have attempted { countInputGuess } times. Try again: \n";
+                    }
+                    else
+                    {
+                        return $"Too high! You have attempted { countInputGuess } times. Try again: \n";
+                    }
+                }
             }
         }
     }
