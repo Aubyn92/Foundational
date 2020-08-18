7f70140da96750c6c5345b29de88be9d986c607f
Aubyn92 <livstaubyn92@gmail.com>
2020-08-18 15:08:54 +1000

    Task 7 completed for printing multiplication table up to 12 numbers.

--- basic-coding/Program.cs
+++ basic-coding/Program.cs
@@ -55,28 +55,37 @@ namespace basic_coding
             //    Console.ReadLine();
 
             // Task 6: Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n:  
-            Console.WriteLine("Please enter a number: ");
-            string number = Console.ReadLine();
-            int n = Convert.ToInt32(number);
-            Console.WriteLine("Choose 1 for the sum of your number, or 2 for the factorial of your number");
-            string chosen = Console.ReadLine();
+            //    Console.WriteLine("Please enter a number: ");
+            //    string number = Console.ReadLine();
+            //    int n = Convert.ToInt32(number);
+            //    Console.WriteLine("Choose 1 for the sum of your number, or 2 for the factorial of your number");
+            //    string chosen = Console.ReadLine();
 
-            int result = -1;
+            //    int result = -1;
 
-            if (chosen == "1")
+            //    if (chosen == "1")
+            //    {
+            //       result = 1;
+            //       for (int i = n; i >= 1; i--) //loop for calculating factorial
+            //       result *= i;
+            //    }
+            //    else if (chosen == "2")
+            //    {
+            //       result = 0;
+            //       for (int i = n; i >= 1; i--)  //loop for calculating sum
+            //       result += i;
+            //    }
+            //    Console.WriteLine(result);
+            //    Console.ReadLine();
+
+            // Task 7: Write a program that prints a multiplication table for numbers up to 12:
+            int num;
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= 12; i++)
             {
-                result = 1;
-                for (int i = n; i >= 1; i--) //loop for calculating factorial
-                result *= i;
+                Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
             }
-            else if (chosen == "2")
-            {
-                result = 0;
-                for (int i = n; i >= 1; i--)  //loop for calculating sum
-                result += i;
-            }
-            Console.WriteLine(result);
-            Console.ReadLine();
         }
     }
 }
