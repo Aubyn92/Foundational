374c131d6f1799425edc09ad9c6c34455dc69e82
Aubyn92 <livstaubyn92@gmail.com>
2020-08-18 13:39:15 +1000

    Task 5 completed for printing the sum of numbers 1 to n if the users input number is a multiple of three or five.

--- basic-coding/Program.cs
+++ basic-coding/Program.cs
@@ -26,15 +26,33 @@ namespace basic_coding
             //    Console.WriteLine($"Your name is incorrect. Rename yourself please.");
 
             // Task 4: Write a program that asks the user for a number n and prints the sum of the numbers 1 to n:
-                   int i, sum = 0, n;
-                   Console.WriteLine("Please enter a number: ");
-                   n = int.Parse(Console.ReadLine());
-                   for (i = 0; i <= n; i++) 
-                   {
-                       sum = sum + i;
-                   }
-                   Console.WriteLine("\nSum of numbers: " + sum);
-                   Console.ReadLine();
+            //    int i, sum = 0, n;
+            //    Console.WriteLine("Please enter a number: ");
+            //    n = int.Parse(Console.ReadLine());
+            //    for (i = 0; i <= n; i++) 
+            //    {
+            //        sum = sum + i;
+            //    }
+            //    Console.WriteLine("\nSum of numbers: " + sum);
+            //    Console.ReadLine();
+
+            // Task 5: Write a program that asks the user for a number n and prints the sum of the numbers 1 to n if the number is a multiple of three or five:
+                int i, sum = 0, n;
+                Console.WriteLine("Please enter a number: ");
+                n = int.Parse(Console.ReadLine());
+                for (i = 0; i <= n; i++) 
+                {
+                    sum = sum + i;
+                }
+                if (sum % 3 == 0 || sum % 5 == 0)
+                {
+                    Console.WriteLine("\nSum of numbers: " + sum);
+                }
+                else 
+                {
+                    Console.WriteLine("Not divisible by 3 and 5");
+                }
+                Console.ReadLine(); 
         }
     }
 }
