c68d6669662d7126bbdd93974c251bfd4d18f867
Aubyn92 <livstaubyn92@gmail.com>
2020-08-19 17:42:41 +1000

    if/else statements for displaying either 'fizz', 'buzz', or theintegers between numbers 1-100.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,8 +10,17 @@ namespace FizzBuzz
             // Write a program that prints the numbers from 1 to 100. 
             // But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
             // For numbers which are multiples of both three and five print “FizzBuzz”."
-            
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz!");
+                else
+                    Console.WriteLine(i);
+            Console.ReadLine();
         }
     }
 }
+
