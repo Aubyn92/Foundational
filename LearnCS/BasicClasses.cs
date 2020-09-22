16b7e7488669116e1f6b169d1a3dc1302396b651
Aubyn92 <livstaubyn92@gmail.com>
2020-09-22 11:00:06 +1000

    class properties and basic classes exercises completed.

--- LearnCS/BasicClasses.cs
+++ LearnCS/BasicClasses.cs
@@ -4,9 +4,23 @@ namespace LearnCS
 {
     class BasicClasses
     {
-        static void Main(string[] args)
+        int numTires = 4;
+        int year = 2000;
+        bool runs = true;
+        public void Car()
         {
-            Console.WriteLine("Hello World!");
+            var car1 = new BasicClasses();
+            car1.Car();
+
+            var car2 = new BasicClasses();
+            car2.Car();
+
+            var car3 = new BasicClasses();
+            car3.Car();
+
+            Console.WriteLine(car1.numTires);
+            Console.WriteLine(car2.year);
+            Console.WriteLine(car3.runs);
         }
     }
 }
