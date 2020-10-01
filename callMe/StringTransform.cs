74e2dd0dc8748da3796d92e3687c4b3d4da988e3
Aubyn92 <livstaubyn92@gmail.com>
2020-10-02 01:38:40 +1000

    added new test files for whitespace and dashes

--- callMe/StringTransform.cs
+++ callMe/StringTransform.cs
@@ -8,9 +8,19 @@ namespace callMe
     {
         public string TransformPhoneNumber (string phoneNumber)
         {
-            return Regex.Replace(phoneNumber, @"[^\w\s\D]", "");
-            // return Regex.Replace(phoneNumber, @"[/^\(?(?:\+?61|0)(?:(?:2\)?[ -]?(?:3[ -]?[38]|[46-9][ -]?[0-9]|5[ -]?[0-35-9])|3\)?(?:4[ -]?[0-57-9]|[57-9][ -]?[0-9]|6[ -]?[1-67])|7\)?[ -]?(?:[2-4][ -]?[0-9]|5[ -]?[2-7]|7[ -]?6)|8\)?[ -]?(?:5[ -]?[1-4]|6[ -]?[0-8]|[7-9][ -]?[0-9]))(?:[ -]?[0-9]){6}|4\)?[ -]?(?:(?:[01][ -]?[0-9]|2[ -]?[0-57-9]|3[ -]?[1-9]|4[ -]?[7-9]|5[ -]?[018])[ -]?[0-9]|3[ -]?0[ -]?[0-5])(?:[ -]?[0-9]){5})$/]");
-            return Regex.Replace(phoneNumber, @"[^[0-9]{10}$", " ");
+            try
+            {
+                return Regex.Replace(phoneNumber, @"[\\s\\-()]", "");
+                // return Regex.Replace(phoneNumber,@"[^1?[\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}]", "");
+                // return Regex.Replace(phoneNumber, @"[^\w\.@-]", "", RegexOptions.None, TimeSpan.FromSeconds(1.5)).Trim();
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return "";
+            }
+            // return Regex.Replace(phoneNumber, @"[^\w\s\D]", "");
+            // return Regex.Replace(phoneNumber,@"[^1?[\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}]", "");
+            // return Regex.Replace(phoneNumber, @"[^()\\s-]+", "").Trim();
         }
     }
 }
